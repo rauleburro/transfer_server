1de4de9f8aaa229eae78313b6832550aa903fe30
Raúl <raul@bucksapp.co>
2023-09-28 00:27:37 -0400

    #2 Configure Apollo Server

---
+++ src/graphql/typeDefs.ts
@@ -0,0 +1,7 @@
+const typeDefs: string = `#graphql
+    type Query {
+        hello: String
+    }
+    `;
+
+export default typeDefs;
--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,15 @@ import './pre-start'; // Must be the first import
 import logger from 'jet-logger';
 
 import EnvVars from '@src/constants/EnvVars';
-import server from './server';
-
+import initializeApp from './server';
 
 // **** Run **** //
 
-const SERVER_START_MSG = ('Express server started on port: ' + 
-  EnvVars.Port.toString());
+(async () => {
+  const SERVER_START_MSG =
+    'Express server started on port: ' + EnvVars.Port.toString();
 
-server.listen(EnvVars.Port, () => logger.info(SERVER_START_MSG));
+  const server = await initializeApp();
+
+  server.listen(EnvVars.Port, () => logger.info(SERVER_START_MSG));
+})();
